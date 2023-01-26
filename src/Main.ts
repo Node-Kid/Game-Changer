9ed8e95d9c4ada4cb186b7c1a90b305b78674b78
will weiszhaar <weiszhaarwill@gmail.com>
2023-01-25 22:40:05 -0500

    update main.ts

--- src/Main.ts
+++ src/Main.ts
@@ -2,7 +2,6 @@ import { TestCard } from "./ts/game/cards/TestCard";
 import { Root } from "./ts/Root";
 import testCardUrl from "../images/cards/test_card.jpg";
 import { Draw } from "./ts/game/modifiers/DrawModifier";
-import { GameEvent } from "./ts/game/core/GameEvent";
 import { CardDiedEvent } from "./ts/game/events/CardDiedEvent";
 import { EventPayload } from "./ts/game/events/EventPayload";
 const canvas = document.getElementById("gameView") as HTMLCanvasElement;
